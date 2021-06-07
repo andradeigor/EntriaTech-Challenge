1bafee00e0595bb5acd88de047fdca6fae2e0e13
andradeigor <ig.vp@hotmail.com>
2021-06-07 17:29:50 -0300

    finished the basic from the homepage

--- web/src/components/LoginCard/index.js
+++ web/src/components/LoginCard/index.js
@@ -1,8 +1,45 @@
-import { CardContainer, Card } from "./styled";
+import {
+  CardContainer,
+  Card,
+  LogoContainer,
+  LogoImage,
+  FormContainer,
+  Form,
+  FormInput,
+  ResetPasswordContainer,
+  ResetPasswordText,
+  ButtonsContainer,
+  LoginButton,
+  LoginButtonText,
+  RegisterButton,
+  RegisterButtonText,
+} from "./styled";
+import LogoPath from "../../assets/logo.svg";
 const LoginCard = () => {
   return (
     <CardContainer>
-      <Card></Card>
+      <Card>
+        <LogoContainer>
+          <LogoImage src={LogoPath} />
+        </LogoContainer>
+        <FormContainer>
+          <Form>
+            <FormInput placeholder="Email..." type="email"></FormInput>
+            <FormInput placeholder="Senha..." type="password"></FormInput>
+            <ResetPasswordContainer>
+              <ResetPasswordText>Esqueceu sua senha?</ResetPasswordText>
+            </ResetPasswordContainer>
+            <ButtonsContainer>
+              <LoginButton>
+                <LoginButtonText>Login</LoginButtonText>
+              </LoginButton>
+              <RegisterButton>
+                <RegisterButtonText>Cadastre-se</RegisterButtonText>
+              </RegisterButton>
+            </ButtonsContainer>
+          </Form>
+        </FormContainer>
+      </Card>
     </CardContainer>
   );
 };
--- web/src/styles/themes/dark.js
+++ web/src/styles/themes/dark.js
@@ -3,8 +3,11 @@ export default {
   colors: {
     primary: "#303030",
     secondary: "#BB86FC",
+    subButton: "#404040",
 
     background: "#121212",
     text: "#FFFFFF",
+    backgroundInput: "#EDE7F6",
+    subtext: "#8F8989",
   },
 };
