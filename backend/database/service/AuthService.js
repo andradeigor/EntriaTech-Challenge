f2504eebd037781f6951606ee73214ec25206dfd
andradeigor <ig.vp@hotmail.com>
2021-06-07 12:15:55 -0300

    new pattern added to controller and service

--- backend/database/service/AuthService.js
+++ backend/database/service/AuthService.js
@@ -5,15 +5,11 @@ module.exports = {
   async AuthLogin(req, res) {
     const { email, password } = req.body;
     const user = await UserModel.findOne({ where: { email } });
-    if (!user) {
-      return res.status(400).json({ error: "Invalid Email or Password" });
-    }
+    return user && password ? await this.GetToken(user, password) : false;
+  },
+  async GetToken(user, password) {
     const login = await UtilsBcrypt.decript(password, user.password);
-    if (login) {
-      const token = await Utilsjwt.encript(user.id);
-      return res.json({ token });
-    } else {
-      return res.status(400).json({ error: "Invalid Email or Password" });
-    }
+    const token = login ? await Utilsjwt.encript(user.id) : false;
+    return token;
   },
 };
--- backend/server/controller/AuthController.js
+++ backend/server/controller/AuthController.js
@@ -1,6 +1,9 @@
 const AuthService = require("../../database/service/AuthService");
 module.exports = {
   async LoginUser(req, res) {
-    await AuthService.AuthLogin(req, res);
+    const token = await AuthService.AuthLogin(req, res);
+    token
+      ? res.json({ token })
+      : res.status(400).json({ error: "Invalid Email or Password" });
   },
 };
