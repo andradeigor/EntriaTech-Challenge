ab0472989e42c8a01757b71a33a2f804f16cbf7f
andradeigor <ig.vp@hotmail.com>
2021-06-08 15:56:05 -0300

    added some validations to the page

--- web/src/components/LoginCard/index.js
+++ web/src/components/LoginCard/index.js
@@ -15,7 +15,14 @@ import {
   RegisterButtonText,
 } from "./styled";
 import LogoPath from "../../assets/logo.svg";
+import { Formik } from "formik";
+import * as yup from "yup";
+
 const LoginCard = () => {
+  const schema = yup.object({
+    email: yup.string().required().email(),
+    password: yup.string().min(6).required(),
+  });
   return (
     <CardContainer>
       <Card>
@@ -23,21 +30,42 @@ const LoginCard = () => {
           <LogoImage src={LogoPath} />
         </LogoContainer>
         <FormContainer>
-          <Form>
-            <FormInput placeholder="Email..." type="email"></FormInput>
-            <FormInput placeholder="Senha..." type="password"></FormInput>
-            <ResetPasswordContainer>
-              <ResetPasswordText>Esqueceu sua senha?</ResetPasswordText>
-            </ResetPasswordContainer>
-            <ButtonsContainer>
-              <LoginButton>
-                <LoginButtonText>Login</LoginButtonText>
-              </LoginButton>
-              <RegisterButton>
-                <RegisterButtonText>Cadastre-se</RegisterButtonText>
-              </RegisterButton>
-            </ButtonsContainer>
-          </Form>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values) => console.log(values)}
+            validationSchema={schema}
+          >
+            {({ values, errors, touched, handleChange, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <FormInput
+                  placeholder="Email..."
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                ></FormInput>
+
+                <FormInput
+                  placeholder="Senha..."
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                ></FormInput>
+                <ResetPasswordContainer>
+                  <ResetPasswordText>Esqueceu sua senha?</ResetPasswordText>
+                </ResetPasswordContainer>
+                <ButtonsContainer>
+                  <LoginButton type="submit">
+                    <LoginButtonText>Login</LoginButtonText>
+                  </LoginButton>
+                  <RegisterButton>
+                    <RegisterButtonText>Cadastre-se</RegisterButtonText>
+                  </RegisterButton>
+                </ButtonsContainer>
+              </Form>
+            )}
+          </Formik>
         </FormContainer>
       </Card>
     </CardContainer>
